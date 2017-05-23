23ff3ca4b65844f15211bf2475eb7f82618228cc
Jeffrey Smeele <jsmeele@gmail.com>
2017-05-23 21:49:56 +0200

    First commit

---
+++ R/convertTextToNum.R
@@ -0,0 +1,13 @@
+#' A Convert function
+#'
+#' This function allows you to cast text to a number.
+#' @param String.
+#' @keywords ToInt,convertTextToNum
+#' @export
+#' @examples
+#' convertTextToNum(string)
+
+convertTextToNum <- function(string)
+{
+  int <- as.integer(string)
+}
